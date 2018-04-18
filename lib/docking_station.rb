cb3e53010465fd102c5d8bb973b3da31add688b1
Possed <r0xana.pirvu@yahoo.com>
2018-04-18 17:56:35 +0100

    starting to increase the capacity of the docking station

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -2,6 +2,10 @@ require './lib/bike'
 class DockingStation
   attr_accessor :bike
 
+  def initialize
+    @bikes = Array.new(20)
+  end
+
   def release_bike
     raise "No bikes available" unless @bike
     @bike
@@ -9,6 +13,6 @@ class DockingStation
 
   def dock_bike(bike)
     @bike = bike
-    raise "Docking station full" unless @bike == nil 
+    raise "Docking station full" unless @bike == nil
   end
 end
