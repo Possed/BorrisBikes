76bfa3d891c274676c790bef534df334cf34fafe
Possed <r0xana.pirvu@yahoo.com>
2018-04-18 12:15:26 +0100

    passed the test for bike working when station released bike

--- spec/bike_spec.rb
+++ spec/bike_spec.rb
@@ -1,9 +1,11 @@
 require 'bike'
 
 describe Bike do
-bike = Bike.new
+
+
 it "responds to is working" do
-  expect(bike).to respond_to(:is_working?)
+  bike = Bike.new
+  expect(bike).to respond_to(:working?)
 end
 
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,7 +1,14 @@
 require 'docking_station'
+require './lib/bike'
 describe DockingStation do
   station = DockingStation.new
+
   it "responds to release_bike" do
     expect(station).to respond_to(:release_bike)
   end
+
+  it "gets a bike when asked to release bike" do
+    expect(station.release_bike).to be_instance_of(Bike)
+  end
+
 end
